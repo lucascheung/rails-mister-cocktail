84e152aad595a75f172bf8e69d7237caf59fd229
Lucas Cheung <lucascheungls@gmail.com>
2019-02-14 14:39:30 +0100

    seb please fix this

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'json'
+require 'open-uri'
+
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+user_serialized = open(url).read
+user = JSON.parse(user_serialized)
+
+user['drinks'][0].each do |key, ingredient|
+  puts ingredient
+end
